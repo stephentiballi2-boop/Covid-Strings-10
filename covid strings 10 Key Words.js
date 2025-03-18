dcd613e90810872394d66f850a3b8322e2b01cf7
SGTIBALCOVIDSTRINGS <stephen.tiballi@daemen.edu>
2025-03-18 18:52:26 -0400

    Create covid strings 10 Key Words.js
    will finish later.

---
+++ covid strings 10 Key Words.js
@@ -0,0 +1,223 @@
+doc.type.html
+script
+filename=covid strings 10 key word.js
+covid strings 10 key word list=[treatments not absolute,doing nothing result in failure,younger people warned this in peril,pacing one treatment,having a conversation post covid is difficult,finding out exactly and when covid began,see problems with politics today,operation speed,cares and build back better acts,infrastructure bill public trust post-covid,delivering vaccines,solution inflation reduction act,to prove were all in this together showing it like,if fraud exists action should be taken,individual states not acting like,operation warp speed warp,democracy toung people covid response,oranges of covid,
+string=””
+for string in covid strings search word list
+select characters
+edit diffuse property
+desired color=green
+covid strings 10 Reference List=[a:can anyone take paxlovid for covid?doctors explain.(msn.com),b:the coronavirus revealed americas failures-the atlantic,c:americas flawed democracy:the five key areas where it is failing |us elections 2020 |d:the guardian,young americans are raising alarms about the state of u.s.democracy in a new poll:npr,e:for long covid fatigue,a strategy called pacing helps,but at a cost (msn.com),f:10 best saturday night live sketches of 2021,ranked |mashable,g:post-quarantine conversation-snl-youtube,h:the covid origins debate -the new york times (nytimes.com),i:no generation without representation:a survey of young americans (dataforprogress.org),j:report finds democracy is declining in the u.s. with trump,capitol attack cited:npr,k:operation warp speed federal efforts to accelerate covid-19 vaccine and therapeutic development |u.s. gao,l:the coronavirus aid,relief,and economic security act:summary of key health provisions |kff,m: whats in the build back better bill the house just passed? |time,
+n:infrastructure package:heres whats in it |cnn politics,o:coronavirus(covid-19)vaccines for developing countries:an equal shot at recovery (oecd.org),p:inflation reduction act-wikipedia,q:christmas vacation-nobodys walking out on this fun old-fashioned family christmas-bing video,r:irs demands 20,000 people pay back money they got during covid (msn.com),s:we are all in this together:human rights and covid-19 response and recovery|united nations,t:our coverage of the coronavirus|the economist,u:democracy,democrats,and young people will save this country|opinion(newsweek.com),v:covid origins -united states house committee on oversight and accountability exclusive:a house subcommittee releases key documents on the pandemic origin paper |the nation,w:chinese app allows small glimpse beyond great firewall |reuters
+asset=this app.assets.find()
+template asset resource 
+entity template instantiate
+entity script foobar=this entity
+entity reparnet (this.entity)
+a=pc.create script(a);
+a.attributes.add(foobar,{type:entity});
+a.prototype.initialize=function()
+console.log(this.foobar.name);
+asset=this app.assets.find()
+template asset resource 
+entity template instantiate
+entity script foobar=this entity
+entity reparnet (this.entity)
+b=pc.create script(b);
+b.attributes.add(foobar,{type:entity});
+b.prototype.initialize=function() 
+console.log(this.foobar.name);
+asset=this app.assets.find()
+template asset resource 
+entity template instantiate
+entity script foobar=this entity
+entity reparnet (this.entity)
+c=pc.create script(c);
+c.attributes.add(foobar,{type:entity});
+c.prototype.initialize=function()
+console.log(this.foobar.name);
+asset=this app.assets.find()
+template asset resource 
+entity template instantiate
+entity script foobar=this entity
+entity reparnet (this.entity)
+d=pc.create script(d);
+d.attributes.add(foobar,{type:entity});
+d.prototype.initialize =function()
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+E=Pc.CreateScript(E);
+E.Attributes.Add(Foobar,{Type: Entity});
+E.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+F=Pc.CreateScript(F);
+F.Attributes.Add(Foobar,{Type: Entity});
+F.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+G=Pc.CreateScript(G);
+G.Attributes.Add(Foobar,{Type: Entity});
+G.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+H=Pc.CreateScript(H);
+H.Attributes.Add(Foobar,{Type: Entity});
+H.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+I=Pc.CreateScript(I);
+I.Attributes.Add(Foobar,{Type: Entity});
+I.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+J=Pc.CreateScript(J);
+J.Attributes.Add(Foobar,{Type: Entity});
+J.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+K=Pc.CreateScript(K);
+K.Attributes.Add(Foobar,{Type: Entity});
+K.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+L=Pc.CreateScript(L);
+L.Attributes.Add(Foobar,{Type: Entity});
+L.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+M=Pc.CreateScript(M);
+M.Attributes.Add(Foobar,{Type: Entity});
+M.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+N=Pc.CreateScript(N);
+N.Attributes.Add(Foobar,{Type: Entity});
+N.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+O=Pc.CreateScript(O);
+O.Attributes.Add(Foobar,{Type: Entity});
+O.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+P=Pc.CreateScript(P);
+P.Attributes.Add(Foobar,{Type: Entity});
+P.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+Q=Pc.CreateScript(Q);
+Q.Attributes.Add(Foobar,{Type: Entity});
+Q.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+R=Pc.CreateScript(R);
+R.Attributes.Add(Foobar,{Type: Entity});
+R.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+S=Pc.CreateScript(S);
+S.Attributes.Add(Foobar,{Type: Entity});
+S.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+T=Pc.CreateScript(T);
+T.Attributes.Add(Foobar,{Type: Entity});
+T.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+U=Pc.CreateScript(U);
+U.Attributes.Add(Foobar,{Type: Entity});
+U.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+Template Asset Resource 
+Entity Template Instanite
+Entity Script Foobar=This Entity
+Entity Reparnet (This.Entity)
+V=Pc.CreateScript(M);
+V.Attributes.Add(Foobar,{Type: Entity});
+V.Prototype.Initialize =Function() {
+Console.Log(This.Foobar.Name);
+Asset=This App .Assets.Find()
+template asset resource 
+entity template instantiate
+entity script foobar=this entity
+entity reparnet (this.entity)
+w=pc.create script(w);
+w.attributes.add(foobar,{type:entity});
+w.prototype.initialize=function()
+console.log(this.foobar.name);
+return filename   
+
+
+
+
